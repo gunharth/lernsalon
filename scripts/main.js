e6752ff38379c983cd8f9d21c20dc063f06f7886
Gunharth Randolf <hello@gunharth.io>
2015-11-05 12:22:51 +0100

    Initial dist commit

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";var cwidth;$(document).ready(function(){cwidth=$("div.circle:eq(0)").width(),$("div.circle").height(cwidth),cwidth=$("div.circle-small:eq(0)").width(),$("div.circle-small").height(cwidth)}),$(window).resize(function(){cwidth=$("div.circle:eq(0)").width(),$("div.circle").height(cwidth),cwidth=$("div.circle-small:eq(0)").width(),$("div.circle-small").height(cwidth)});

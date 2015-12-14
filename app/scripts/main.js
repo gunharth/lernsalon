0a35f9751a1f98298a460d1f22d2c4fd6b0250ce
Gunharth Randolf <hello@gunharth.io>
2015-12-14 22:55:04 +0100

    add dist folder

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,10 @@
 'use strict';
 
 function checkColumnHeights() {
-	//greenBox
-	/*var yellowBoxHeight = $('#yellowBox').height();
-	$('#greenBox').height(yellowBoxHeight);*/
-
-	// whiteBox
 	var rightColheight = $('#rightCol').height();
 	var redBoxHeight = $('#redBox').height();
-	var whiteBoxHeight = rightColheight-redBoxHeight;
-	$('#whiteBox').height(whiteBoxHeight-100);
+	var whiteBoxHeight = rightColheight - redBoxHeight;
+	$('#whiteBox').height(whiteBoxHeight - 100);
 }
 
 $(document).ready(function() {
